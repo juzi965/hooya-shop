c5f08a6d8c12ce66096c0f43fd0bab5a6f8c6a68
juzi <juzi965@outlook.com>
2019-12-16 12:52:31 +0800

    初始化项目

---
+++ shop-api/src/test/java/top/hooya/shop/ShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package top.hooya.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
